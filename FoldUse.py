f4e69e3bc47e671a15264c7389b20f70e7283daf
Black formatter <username@users.noreply.github.com>
2023-07-04 12:10:36 +0000

    Apply black formatted code

--- FoldUse.py
+++ FoldUse.py
@@ -4,13 +4,13 @@ import sublime_plugin
 
 class FoldUseCommand(sublime_plugin.TextCommand):
     def run(self, edit, args=None):
-        if (isNotPhpFile(self.view)):
+        if isNotPhpFile(self.view):
             return
 
         self.view.fold(self.get_use_statements_region())
 
     def get_use_statements_region(self):
-        regions = self.view.find_all('^use .*;$', 0)
+        regions = self.view.find_all("^use .*;$", 0)
 
         if len(regions) == 0:
             return []
@@ -20,16 +20,16 @@ class FoldUseCommand(sublime_plugin.TextCommand):
 
 class FoldUseEventListener(sublime_plugin.EventListener):
     def on_post_save(self, view):
-        if (isNotPhpFile(view)):
+        if isNotPhpFile(view):
             return
 
-        view.run_command('fold_use')
+        view.run_command("fold_use")
 
     def on_load(self, view):
-        if (isNotPhpFile(view)):
+        if isNotPhpFile(view):
             return
 
-        view.run_command('fold_use')
+        view.run_command("fold_use")
 
 
 def isNotPhpFile(view):
